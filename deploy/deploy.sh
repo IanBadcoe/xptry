24b1d06a7a77d3659f5585e5e2dd1cc8f7e4022c
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2019-11-24 14:47:38 +0000

    unzip EE

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SITE="xptry.com"
+EEVERSION="5.3.0"
+
 apt update
 apt upgrade -y
 apt install mysql-server -y
@@ -10,3 +13,9 @@ mysql -e "GRANT ALL PRIVILEGES ON EE.* TO 'ee'@'localhost'"
 
 apt install apache2 -y
 
+apt install unzip
+
+mkdir -p ../www
+unzip -qu installs/ExpressionEngine${EEVERSION}.zip -d ../www
+find ../www \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
+chown $USER:$USER ../www
---
+++ util/be_ian_4_git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Ian Badcoe"
+git config --global user.email "ian_badcoe@yahoo.co.uk"
+
+git config credential.helper store
