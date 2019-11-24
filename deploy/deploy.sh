1b0e03d35d0f53084e4e601cda668766273ea2dd
Ubuntu <ubuntu@ip-172-31-34-155.eu-west-1.compute.internal>
2019-11-24 12:27:00 +0000

    fixed not trying to create user twice

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,6 @@ apt update
 apt upgrade -y
 apt install mysql-server -y
 
-mysql -e "CREATE USER ee@localhost IDENTIFIED BY 'eepwd'"
+mysql -e "CREATE USER IF NOT EXISTS ee@localhost IDENTIFIED BY 'eepwd'"
 mysql -e "CREATE DATABASE IF NOT EXISTS EE"
 mysql -e "GRANT ALL PRIVILEGES ON EE.* TO 'ee'@'localhost'"
