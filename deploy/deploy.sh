50f3d4adb04a945cfad01f07420ba4e44b7fb4b9
Ubuntu <ubuntu@ip-172-31-34-155.eu-west-1.compute.internal>
2019-11-24 12:25:06 +0000

    deploy mysql setup

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 apt update
-apt install mysql-server
+apt upgrade -y
+apt install mysql-server -y
 
-# Kill the anonymous users
-mysql -e "DROP USER ''@'localhost'"
-# Because our hostname varies we'll use some Bash magic here.
-mysql -e "DROP USER ''@'$(hostname)'"
-# Kill off the demo database
-mysql -e "DROP DATABASE test"
-# Make our changes take effect
-mysql -e "FLUSH PRIVILEGES"
-# disallow remove root access
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+mysql -e "CREATE USER ee@localhost IDENTIFIED BY 'eepwd'"
+mysql -e "CREATE DATABASE IF NOT EXISTS EE"
+mysql -e "GRANT ALL PRIVILEGES ON EE.* TO 'ee'@'localhost'"
