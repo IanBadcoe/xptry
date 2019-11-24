441da03d85284f594fc70b686deccce5891fa441
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2019-11-24 12:32:21 +0000

    initial apache setup

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,3 +7,6 @@ apt install mysql-server -y
 mysql -e "CREATE USER IF NOT EXISTS ee@localhost IDENTIFIED BY 'eepwd'"
 mysql -e "CREATE DATABASE IF NOT EXISTS EE"
 mysql -e "GRANT ALL PRIVILEGES ON EE.* TO 'ee'@'localhost'"
+
+apt install apache2 -y
+
