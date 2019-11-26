b2db6bfe673cc1419e01307da497f93a91aa6b02
Ubuntu <ubuntu@ip-172-31-34-238.eu-west-1.compute.internal>
2019-11-26 20:39:58 +0000

    testing as far as php installation and running compatibility wizard

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,23 +14,31 @@ mysql -e "GRANT ALL PRIVILEGES ON EE.* TO 'ee'@'localhost'"
 
 apt install apache2 -y
 
+apt install php libapache2-mod-php php-mysql php-curl php-zip -y
+
 apt install unzip
 
-mkdir -p ~/www
-unzip -qu installs/ExpressionEngine${EEVERSION}.zip -d ~/www
-find ~/www \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
-chown $USER:$USER ~/www
+mkdir ~/wizard
+rm /var/www/html/index.html
+curl https://ellislab.com/asset/file/ee_server_wizard.zip > /var/www/html/wizard.zip
+unzip -o /var/www/html/wizard.zip -d /var/www/html
+rm /var/www/html/wizard.zip
 
-ln -sfn ~/www /var/www/${SITE}
+#sudo apt install php${PHPVERSION}-fpm php${PHPVERSION}-common php${PHPVERSION}-mysql php${PHPVERSION}-xml php${PHPVERSION}-xmlrpc php${PHPVERSION}-curl php${PHPVERSION}-gd php${PHPVERSION}-imagick php${PHPVERSION}-cli php${PHPVERSION}-dev php${PHPVERSION}-imap php${PHPVERSION}-mbstring php${PHPVERSION}-soap php${PHPVERSION}-zip php${PHPVERSION}-bcmath -y
+#a2enmod proxy-fcgi setenvif
+#a2enconf php7.2-fpm
 
-apt install php -y
-sudo apt install php${PHPVERSION}-fpm php${PHPVERSION}-common php${PHPVERSION}-mysql php${PHPVERSION}-xml php${PHPVERSION}-xmlrpc php${PHPVERSION}-curl php${PHPVERSION}-gd php${PHPVERSION}-imagick php${PHPVERSION}-cli php${PHPVERSION}-dev php${PHPVERSION}-imap php${PHPVERSION}-mbstring php${PHPVERSION}-soap php${PHPVERSION}-zip php${PHPVERSION}-bcmath -y
-a2enmod proxy-fcgi setenvif
-a2enconf php7.2-fpm libapache2-mod-php php-mysql
+#apt install ruby -y
+#erb site="${SITE}" -T - files/apache.conf.erb > /etc/apache2/sites-available/${SITE}.conf
 
-apt install ruby -y
-erb site="${SITE}" -T - files/apache.conf.erb > /etc/apache2/sites-available/${SITE}.conf
-a2ensite ${SITE}.conf
-a2dissite 000-default.conf
+#mkdir -p ~/www
+#unzip -qu installs/ExpressionEngine${EEVERSION}.zip -d ~/www
+#find ~/www \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
+#chown $USER:$USER ~/www
+
+#ln -sfn ~/www /var/www/${SITE}
+
+#a2ensite ${SITE}.conf
+#a2dissite 000-default.conf
 systemctl restart apache2
 systemctl restart mysql.service
