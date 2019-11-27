0d8178f36a14a68a996b5e500883e2f0b8117b2d
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2019-11-27 14:42:29 +0000

    deploy working

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ PHPVERSION="7.2"
 
 apt update
 apt upgrade -y
+apt autoremove -y
 apt install mariadb-server -y
 
 mysql -e "CREATE USER IF NOT EXISTS ee@localhost IDENTIFIED BY 'eepwd'"
@@ -16,29 +17,31 @@ apt install apache2 -y
 
 apt install php libapache2-mod-php php-mysql php-curl php-zip php-mbstring php-gd -y
 
-apt install unzip
+apt install unzip curl -y
 
-mkdir ~/wizard
-rm /var/www/html/index.html
-curl https://ellislab.com/asset/file/ee_server_wizard.zip > /var/www/html/wizard.zip
-unzip -o /var/www/html/wizard.zip -d /var/www/html
-rm /var/www/html/wizard.zip
+sudo apt install php${PHPVERSION}-fpm php${PHPVERSION}-common php${PHPVERSION}-mysql php${PHPVERSION}-xml php${PHPVERSION}-xmlrpc php${PHPVERSION}-curl php${PHPVERSION}-gd php${PHPVERSION}-imagick php${PHPVERSION}-cli php${PHPVERSION}-dev php${PHPVERSION}-imap php${PHPVERSION}-mbstring php${PHPVERSION}-soap php${PHPVERSION}-zip php${PHPVERSION}-bcmath -y
+#sudo apt install php${PHPVERSION}-fpm -y
+a2enmod proxy-fcgi setenvif
 
-#sudo apt install php${PHPVERSION}-fpm php${PHPVERSION}-common php${PHPVERSION}-mysql php${PHPVERSION}-xml php${PHPVERSION}-xmlrpc php${PHPVERSION}-curl php${PHPVERSION}-gd php${PHPVERSION}-imagick php${PHPVERSION}-cli php${PHPVERSION}-dev php${PHPVERSION}-imap php${PHPVERSION}-mbstring php${PHPVERSION}-soap php${PHPVERSION}-zip php${PHPVERSION}-bcmath -y
-#a2enmod proxy-fcgi setenvif
-#a2enconf php7.2-fpm
+apt install ruby -y
+erb site="${SITE}" phpversion="${PHPVERSION}" -T - files/apache.conf.erb > /etc/apache2/sites-available/${SITE}.conf
+a2ensite ${SITE}.conf
 
-#apt install ruby -y
-#erb site="${SITE}" -T - files/apache.conf.erb > /etc/apache2/sites-available/${SITE}.conf
+rm -rf /var/www/${SITE}
 
-#mkdir -p ~/www
-#unzip -qu installs/ExpressionEngine${EEVERSION}.zip -d ~/www
-#find ~/www \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
-#chown $USER:$USER ~/www
+# to install the EE compatibility wizard
+#mkdir ~/wizard
+#curl https://ellislab.com/asset/file/ee_server_wizard.zip > /var/www/html/wizard.zip
+#unzip -o /var/www/html/wizard.zip -d /var/www/${SITE}
+#rm /var/www/html/wizard.zip
 
-#ln -sfn ~/www /var/www/${SITE}
+mkdir -p ~/www
+unzip -qu installs/ExpressionEngine${EEVERSION}.zip -d ~/www
+find ~/www \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
+chown -R www-data:www-data ~/www
 
-#a2ensite ${SITE}.conf
-#a2dissite 000-default.conf
+ln -sfn ~/www /var/www/${SITE}
+
+a2dissite 000-default.conf
 systemctl restart apache2
 systemctl restart mysql.service
---
+++ deploy/remove_installer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf ../www/system/ee/installer
