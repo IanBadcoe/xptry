d7c772d7af04e7db7109dbcb61ee9d807ea3240b
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2019-11-29 16:24:34 +0000

    remote restore

--- util/restore.sh
+++ util/restore.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 mkdir -p ~/backup_c
+mkdir -p ~/backup
+
 name=${1}.tar.gz
+
 aws s3 cp s3://xptry-backups/${name} ~/backup_c
+
 cat ~/backup_c/${name} | gunzip | tar -C ~/backup -xvf -
+
 if [[ -d ~/backup/www && -e ~/backup/db.sql ]]
 then
   systemctl stop apache2
